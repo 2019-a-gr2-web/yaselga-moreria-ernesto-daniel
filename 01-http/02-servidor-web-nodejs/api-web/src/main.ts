ecc801f5b405295da212b7b82b574b9f4f058c50
Ernesteins <eyaselga@hotmail.com>
2019-05-07 15:58:57 -0500

    favicon e introduccion a html

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
 import {join} from "path";
+import * as express from 'express';
+import * as path from "path";
+import * as favicon from "serve-favicon";
+
 //import * as cookie from 'cookie-parser'; //<-- forma typescript
 var cookieParser = require('cookie-parser'); //<-- forma javascript
 
@@ -11,6 +15,8 @@ async function bootstrap() {
   app.use(cookieParser('secreto para las cookies'));
   app.setViewEngine('ejs');
   app.setBaseViewsDir(join(__dirname,'..','views'));
+  app.use(express.static('public'));
+  app.use(favicon(path.join(__dirname,'..','public','img','DeathStar.ico')));
 
   await app.listen(3000);
 }
